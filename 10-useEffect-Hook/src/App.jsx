5e9f942e55267b54971d26affd1e391e8161a692
Alokkumar-8467 <138556514+Alokkumar-8467@users.noreply.github.com>
2024-01-28 04:28:33 +0530

    Add files via upload

---
+++ 10-useEffect-Hook/src/App.jsx
@@ -0,0 +1,18 @@
+// 10-useEffect-Hook
+
+
+import UseEffectExample from "./UseEffectExample";
+
+
+function App() {
+  
+
+  return (
+    <div>
+    <h1>useEffectExample</h1>
+    <UseEffectExample />
+    </div>
+  );
+}
+
+export default App;
