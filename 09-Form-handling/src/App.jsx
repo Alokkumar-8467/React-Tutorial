0af5f126a6dc9011b4b8108cbb6a2ecf7560542e
Alokkumar-8467 <138556514+Alokkumar-8467@users.noreply.github.com>
2024-01-27 13:02:56 +0530

    Add files via upload

---
+++ 09-Form-handling/src/App.jsx
@@ -0,0 +1,23 @@
+/*
+09-Form-handling
+
+In this we make a form which include these things.
+  * Create A Form
+  * Handle Data Of Inputs
+  * Log Data On Submit
+*/
+import Form from "./Form";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+    <h1>Form Handling Example</h1>
+    <div className="layout">
+      <Form />
+    </div>  
+    </div>
+  )
+}
+
+export default App;
