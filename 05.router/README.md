4ce45fb6579d7366bdbec3edd00eb4bbe41afb22
Alokkumar-8467 <138556514+Alokkumar-8467@users.noreply.github.com>
2024-03-16 18:49:02 +0530

    Add files via upload

---
+++ 05.router/README.md
@@ -0,0 +1,4 @@
+React Router
+
+1. Install npm install react-router-dom.
+2. 
---
+++ 05.router/src/components/Github/Github.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react'
+
+const Github = () => {
+    // useEffect(() => {
+    //     fetch("https://github.com/Alokkumar-8467")
+    //     .then(response)
+    // },[])
+  return (
+    <div className='text-center m-4 bg-gray-500 text-white text-3xl'>Github followers : </div>
+  )
+}
+
+export default Github
